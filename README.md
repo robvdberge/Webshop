58a39995ae87846f07f68498a5018ee21eb0a4c1
Rob van den Berge <robvdberge@gmail.com>
2019-08-25 11:55:06 +0200

    Updated contact page and reconfigured README.md

--- README.md
+++ README.md
@@ -1,12 +1,47 @@
-Using a Udemy course to code a fully functional webshop in PHP using Mysqli.
-Addendum:
+# Webshop Project
+
+A fully working webshop for a generic range of products.
+
+## Description
+
+A fully-functional webshop in PHP.
+Users are able to register, create topics and reply to topics.
+The admin area is secured and has a full CRUD functionality for:
+
+* products
+* copyrights
+* categories
+* social media links
+* Slider images
+* Status page for orders
+* Wishlist
+
+### Uses
+
+* OOP MySQL CRUD engine using MySQLI
+* Full Login functionality (create/register, update, delete account via settings page)
+
+## What I learned in this project
+
+* Using a Admin area to use CRUD functionality for products
+* Payment procedure
+* Added a breadcrumb navigation
+* Shopping Cart functionality
+* Search functionality for DB fields
+* Uploading images, creating unique filenames for them and linking them to products
+
+### Programmed using
+
+* WAMP 3.1.7 64bit
+* MS Visual Studio Code
+
+### Addendum:
+
+* All viewable HTML text is now in Dutch
 * Added a category overview in the category pages and product page
 * Improved responsiveness with category section
 * Changed colorscheme in style.css
+* Wishlist now doesn't need a login
 * added the category menu in preview and category pages
 * Add a breadcrumb navigation on products, category and preview pages
 * improved rounded prices in cart and header
-todo:
-* change db connection to use PDO in stead of Mysqli
-* product page Head section (total amount of products found in db)
-* product page Latest product (maybe each category with a pic?)
--- contact.php
+++ contact.php
@@ -5,8 +5,8 @@
     <div class="content">
     	<div class="support">
   			<div class="support_desc">
-  				<h3>Live Support</h3>
-  				<p><span>24 hours | 7 days a week | 365 days a year &nbsp;&nbsp; Live Technical Support</span></p>
+  				<h3>Ondersteuning</h3>
+  				<p><span>24 uurs ondersteuning &nbsp;&nbsp; Live Technical Support</span></p>
   				<p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
   			</div>
   				<img src="web/images/contact.png" alt="" />
@@ -15,40 +15,40 @@
     	<div class="section group">
 				<div class="col span_2_of_3">
 				  <div class="contact-form">
-				  	<h2>Contact Us</h2>
+				  	<h2>Contact</h2>
 					    <form>
 					    	<div>
-						    	<span><label>NAME</label></span>
+						    	<span><label>Naam</label></span>
 						    	<span><input type="text" value=""></span>
 						    </div>
 						    <div>
-						    	<span><label>E-MAIL</label></span>
+						    	<span><label>Email</label></span>
 						    	<span><input type="text" value=""></span>
 						    </div>
 						    <div>
-						     	<span><label>MOBILE.NO</label></span>
+						     	<span><label>Telefoonnummer</label></span>
 						    	<span><input type="text" value=""></span>
 						    </div>
 						    <div>
-						    	<span><label>SUBJECT</label></span>
+						    	<span><label>Onderwerp</label></span>
 						    	<span><textarea> </textarea></span>
 						    </div>
 						   <div>
-						   		<span><input type="submit" value="SUBMIT"></span>
+						   		<span><input type="submit" value="Verzend"></span>
 						  </div>
 					    </form>
 				  </div>
   				</div>
 				<div class="col span_1_of_3">
       			<div class="company_address">
-				     	<h2>Company Information :</h2>
+				     	<h2>Bedrijfsinformatie :</h2>
 						    	<p>500 Lorem Ipsum Dolor Sit,</p>
 						   		<p>22-56-2-9 Sit Amet, Lorem,</p>
-						   		<p>USA</p>
-				   		<p>Phone:(00) 222 666 444</p>
+						   		<p>Nederland</p>
+				   		<p>Tel:(00) 222 666 444</p>
 				   		<p>Fax: (000) 000 00 00 0</p>
 				 	 	<p>Email: <span>info@mycompany.com</span></p>
-				   		<p>Follow on: <span>Facebook</span>, <span>Twitter</span></p>
+				   		<p>Volg ons op: <span>Facebook</span>, <span>Twitter</span></p>
 				   </div>
 				 </div>
 			  </div>    	
